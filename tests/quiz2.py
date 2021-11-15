9969319bfa8b257bc77f29dded1da3e3fa76f766
Neil Sawhney <Neil@neil-sawhney.com>
2021-11-15 01:01:16 -0500

    test2

--- tests/quiz2.py
+++ tests/quiz2.py
@@ -1,2 +1,15 @@
 from sympy import *
 
+print("----------------------------------")
+print("normal = ", 4*9.8+9.8)
+
+print("----------------------------------")
+alpha = symbols("alpha")
+eq1 = Eq(((9/5)-(1/2))*20 - (9/5)*(9*alpha+20), 17/15*alpha)
+alpha = solve(eq1)[0]
+print("alpha = ", alpha)
+
+print("----------------------------------")
+print("Friction =", 9*alpha+20)
+
+
